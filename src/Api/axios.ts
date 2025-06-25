2f0379649b6b0e49e6ab4aa99002b3149e51cf98
Ali pc Ali <@im.ali@astanaqulov.ru>
2025-06-25 05:27:03 +0300

    update

--- src/Api/axios.ts
+++ src/Api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const url2 = "http://801db791-e5ec-466d-9780-bf4fd5521d93-00-18vfa7yeo31sn.sisko.replit.dev/api"
+const url2 = "https://801db791-e5ec-466d-9780-bf4fd5521d93-00-18vfa7yeo31sn.sisko.replit.dev/api"
 
 export const api = axios.create({
     baseURL: url2,
